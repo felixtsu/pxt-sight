86714bf59e4ca437c72ff9def161a7f5d1092c35
felixtsu <humaeks@gmail.com>
2020-12-08 19:15:29 +0800

    Update pxt.json, sight.ts

--- sight.ts
+++ sight.ts
@@ -1,7 +1,9 @@
-// 在此处添加您的代码
-namespace sight {
+ // 在此处添加您的代码
+namespace sight{
 
-    function judgeByDistance(sprite:Sprite, target:Sprite, range:number) {
+    let showTestingTiles = false
+
+    function distanceInRange(sprite:Sprite, target:Sprite, range:number) {
         return Math.sqrt(Math.pow(sprite.x - target.x, 2) + Math.pow(sprite.y - target.y, 2)) <= range
     }
     
@@ -9,89 +11,125 @@ namespace sight {
         return game.currentScene().tileMap.scale
     }
 
+    function scaleDelta() {
+        return 1 << currentScale()
+    }
 
-    function isWallBetween2(start:Sprite, end:Sprite) {
+    const SPRITE_KIND_TESTING = SpriteKind.create()
+
+    function placeTestSprite(col:number, row:number, h:Image) {
+        if (showTestingTiles) {
+            let testSprite = sprites.create(h, SPRITE_KIND_TESTING)
+            tiles.placeOnTile(testSprite, tiles.getTileLocation(col, row))        
+        }
+    }
+
+
+    function isWallBetween(start:Sprite, end:Sprite) {
+        if (showTestingTiles) {
+            for (let sprite of sprites.allOfKind(SPRITE_KIND_TESTING)) {
+                sprite.destroy()
+            }
+        }
+
         // always from left to right
         if (start.x > end.x) {
             let temp = start
             start = end
-            end = start
+            end = temp
         }
 
         let slope = (end.y - start.y) / (end.x - start.x)
-        let sign = (end.y > start.y) ? 1 : -1 
+        let ySign = (end.y > start.y) ? 1 : -1 
 
         // points at column boundaries
         let boundaryPoints = []
-        for (let x = start.x  + currentScale() - start.x % currentScale(), y = start.y + currentScale() - start.y % currentScale(); x < end.x; x += currentScale(), y+= currentScale()*sign*slope) {
+        for (let x = start.x  + scaleDelta() - start.x % scaleDelta(), y = start.y + (scaleDelta() - start.x % scaleDelta())*slope ; x < end.x; x += scaleDelta(), y+= scaleDelta()*slope) {
             boundaryPoints.push([x, y])
         }
 
-        // for each points on vertical lines, check tile to the right is isWallBetween
+        // for each points on vertical lines, check if tile to the right isObstacle
         for (let boundaryPoint of boundaryPoints) {
-            if (game.currentScene().tileMap.isObstacle(boundaryPoint[0] / currentScale(), boundaryPoint[1] / currentScale())) {
+            let [col, row] = [boundaryPoint[0] >> currentScale(), boundaryPoint[1] >> currentScale() ]
+            placeTestSprite(col, row, img`
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+                c c c c c c c c c c c c c c c c
+            `)
+            if (game.currentScene().tileMap.isObstacle(col, row)) {
                 return true
-            }
+            } 
         }
 
         boundaryPoints = []
+    
+        if (ySign > 0) {
+            let x = start.x + (scaleDelta() - (start.y % scaleDelta())) / slope 
+            let y = start.y - (start.y % scaleDelta())  + scaleDelta() 
 
-        for (let x = start.x  + currentScale() - start.x % currentScale(), y = start.y + currentScale() - start.y % currentScale(); (y - end.y)*sign > 0; y += currentScale()*sign, x+= currentScale()/slope) {
-            boundaryPoints.push([x, y])
+            while((y < end.y)) {
+                boundaryPoints.push([x, y])
+                y += scaleDelta(), 
+                x += scaleDelta()/slope
+            } 
+
+        } else { 
+            let x = start.x - (start.y % scaleDelta()) / slope 
+            let y = start.y - (start.y % scaleDelta())  
+
+            while((y > end.y)) {
+                boundaryPoints.push([x, y])
+                y -= scaleDelta(), 
+                x -= scaleDelta()/slope
+            } 
+
         }
 
-        // for each points on horizontal lines, check tile to the up/down (sign) is isWallBetween
+        // for each points on horizontal lines, check if tile to the up/down (sign) isObstacle
         for (let boundaryPoint of boundaryPoints) {
-            if (game.currentScene().tileMap.isObstacle(boundaryPoint[0] / currentScale(), boundaryPoint[1] / currentScale() + sign>0?1:0 )) {
+            let [col, row] = [boundaryPoint[0] >> currentScale(), (boundaryPoint[1] >> currentScale()) + (ySign>0?0:-1) ]
+            placeTestSprite(col, row, img`
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+                2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2
+            `)
+            if (game.currentScene().tileMap.isObstacle(col, row)) {
                 return true
-            }
+            } 
         }
 
         return false
     }
 
-
-    function isWallBetween(sprite:Sprite, target:Sprite) {
-        const currentTileMap = game.currentScene().tileMap
-
-        const angle = Math.atan2(target.y - sprite.y, target.x - sprite.x)
-
-        let [startRow, startCol] = [sprite.x / currentTileMap.scale, sprite.y / currentTileMap.scale]
-        let [endRow, endCol] = [target.x / currentTileMap.scale, target.y / currentTileMap.scale]
-
-        // always from left to right
-        if (startRow > endRow) {
-            let tempRow = endRow
-            endRow = startRow
-            startRow = endRow
-
-            let tempCol = endCol
-            endCol = startCol
-            startCol = endCol
-            
-        }
-
-        while (startRow != endRow && startCol != endCol) {
-            if (game.currentScene().tileMap.isObstacle(startRow, startCol) ) {
-                return true;
-            }
-
-            if (true) {
-                startCol += 1
-            } else {
-                endRow += endRow > startRow ? 1 : -1  
-            }
-            
-            
-
-        }
-        return false;
-    }
-    
-
-
+    //%block="can %sprite see %target, range %range, %omitWalls "
     export function isInSight(sprite:Sprite, target:Sprite, range:number, omitWalls:boolean) {
-        if (!judgeByDistance(sprite, target, range)) {
+        if (!distanceInRange(sprite, target, range)) {
             return false 
         }
         
@@ -99,7 +137,7 @@ namespace sight {
             return true
         }
 
-        return isWallBetween2(sprite, target)
+        return !isWallBetween(sprite, target)
         
     }
 
